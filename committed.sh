a0e644d1fc77357d9c8460129b7bd01caac8d6f3
rmachoka <ron.machoka@cellulant.io>
2022-07-14 10:36:36 +0300

    Whip it, whip it good!

--- committed.sh
+++ committed.sh
@@ -3,8 +3,8 @@ NUMBER=$(($RANDOM % 23))
 
 for ((run=1; run <= NUMBER + 1; run++))
 do
-  echo `/usr/games/fortune` > ~/pusher/file.txt
-  /usr/bin/git -C ~/pusher/ add . -A
-  /usr/bin/git -C ~/pusher/ commit -m "`/usr/games/fortune -sn 32`"
+  echo `fortune` > ~/greenery/file.txt
+  /usr/bin/git -C ~/greenery/ add . -A
+  /usr/bin/git -C ~/greenery/ commit -m "`fortune -sn 32`"
 done
 
